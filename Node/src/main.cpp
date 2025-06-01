bf7de8f150efe7fb496d8c66a4b941a3e067b735
cris-ctrl <cristianwalter21@gmail.com>
2025-06-01 17:20:06 -0300

    added esp-now communication

--- Node/src/main.cpp
+++ Node/src/main.cpp
@@ -1,34 +1,56 @@
 #include <DHT.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 
 // Configuration
 #define DHTPIN 2     // GPIO2 on ESP-01 (physical pin 5)
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
+//###########
+uint8_t mac09[] = {0x2C, 0xF4, 0x32, 0x8C, 0x09, 0xBF}; // mac of the controller
+
+
+//packet structures
+struct SensorPacket {
+  char name[4];
+  float temperature;
+  float humidity;
+};
+
+struct AckPacket {
+  char name[4];
+};
+
+
+
 
 void setup() {
   Serial.begin(9800);
   dht.begin();
-  Serial.println("\nESP-01 DHT11 Sensor Test");
+  WiFi.mode(WIFI_STA);  // ESP-NOW works only in STA or AP mode
+
+  if (esp_now_init() != 0) { //initializes esp-now while testing if it actually worked
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER); //controller only sends, slave only recieves, combo cant decide if hes a top or bottom (its both)
+  esp_now_add_peer(mac09, ESP_NOW_ROLE_SLAVE, 1, NULL, 0); // adds esp09 as a slave peer, sending on channel 1 of 13, without encription
+
 }
 
 void loop() {
-  delay(2000);  // Wait between measurements (DHT11 max rate is 1Hz)
+  delay(5000);  // Wait between measurements (DHT11 max rate is 1Hz)
   
   float humidity = dht.readHumidity();
   float tempC = dht.readTemperature();  // Celsius
   
-  // Check if readings failed
-  if (isnan(humidity) || isnan(tempC)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+  //look how cute :3 were wrapping our data into a neat lil packet (please help im going insane)
+  SensorPacket Data;
+  Data.temperature = tempC;
+  Data.humidity = humidity;
 
-  // Print results
-  Serial.print("Temperature: ");
-  Serial.print(tempC);
-  Serial.print("°C\t");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
+  esp_now_send(mac09, (uint8_t *)&Data, sizeof(Data)); //(uint8_t *)&Data turns the packet struct into a byte array, and sends it to mac09
+
 }
