df24e320c4ed0771471ea4c2898a0124132eb82d
cris-ctrl <cristianwalter21@gmail.com>
2025-06-02 11:25:55 -0300

    removed unused code, and added dashboard again

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <ESP8266WebServer.h>
 
-// Peer MAC addresses
-uint8_t macA4[] = {0xA4, 0xE5, 0x7C, 0xBB, 0xE9, 0xFC};
-uint8_t mac2C[] = {0x2C, 0xF4, 0x32, 0x8C, 0x09, 0xBF};  // Corrected to match node's controller MAC
 
 // network config
 char NN[] = "Fiodor";
 char PW[] = "dotoievski";
+ESP8266WebServer server(80);
 
 struct SensorPacket {
   char name[4];
@@ -35,10 +34,32 @@ void onReceive(uint8_t *mac, uint8_t *data, uint8_t len){
   }
 }
 
+void handleRoot() {
+  String page = "<html><head><meta charset='utf-8'><meta http-equiv='refresh' content='2'><title>ESP-SAP</title></head><body>";
+  page += "<h1>Sensor Data</h1>";
+  page += "<p><b>Device:</b> " + String(recievedData.name) + "</p>";
+  page += "<p><b>Humidity:</b> " + String(recievedData.humidity) + "%</p>";
+  page += "<p><b>Temperature:</b> " + String(recievedData.temperature) + "°C</p>";
+  page += "</body></html>";
+
+  server.send(200, "text/html", page);
+}
+
+
+
 void setup(){
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+  WiFi.begin(NN, PW);
+
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   if (esp_now_init() != 0) {
     Serial.println("ESP-NOW init failed!");
@@ -51,7 +72,56 @@ void setup(){
   Serial.println("workingig");
 
 
-}
-void loop(){
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Web server started!");
+
 
 }
+void loop(){
+  server.handleClient();
+}
+
+
+/*
+
+// Serve data on web page
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // Connect to home Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("ESP-NOW init failed!");
+    return;
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(onReceive);
+
+  // Setup web server
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Web server started!");
+}
+
+void loop() {
+  server.handleClient();
+}
+*/
