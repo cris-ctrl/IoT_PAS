f5c5661008edf4ab3f1c114c8f3f258ec4d5992a
cris-ctrl <cristianwalter21@gmail.com>
2025-06-02 10:32:18 -0300

    recieving packets now :3

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -15,3 +15,43 @@ struct SensorPacket {
   float temperature;
   float humidity;
 };
+
+SensorPacket recievedData;
+
+void onReceive(uint8_t *mac, uint8_t *data, uint8_t len){
+  if(len == sizeof(SensorPacket)){
+    memcpy(&recievedData, data, sizeof(SensorPacket));
+
+    Serial.println("Recieved from: ");
+    Serial.println(recievedData.name);
+    Serial.println("moisty level: ");
+    Serial.println(recievedData.humidity);
+    Serial.println("Temperature: ");
+    Serial.println(recievedData.temperature);
+
+  }else{
+    Serial.println("packet size error, trying to print anyway");
+    Serial.println(recievedData.humidity);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  if (esp_now_init() != 0) {
+    Serial.println("ESP-NOW init failed!");
+    return;
+  }
+
+  // Registra função para tratar pacotes recebidos
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE); // ESP8266
+  esp_now_register_recv_cb(onReceive);
+  Serial.println("workingig");
+
+
+}
+void loop(){
+
+}
