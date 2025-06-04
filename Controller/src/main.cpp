eab060d2f1cda45a93c6f349c9936722974c06fc
cris-ctrl <cristianwalter21@gmail.com>
2025-06-04 11:10:45 -0300

    organized code, added FS back

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -2,9 +2,9 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>  // Include SPIFFS library
 
-
-// network config
+// Network config
 char NN[] = "Fiodor";
 char PW[] = "dotoievski";
 ESP8266WebServer server(80);
@@ -17,111 +17,71 @@ struct SensorPacket {
 
 SensorPacket recievedData;
 
-void onReceive(uint8_t *mac, uint8_t *data, uint8_t len){
-  if(len == sizeof(SensorPacket)){
+void onReceive(uint8_t *mac, uint8_t *data, uint8_t len) {
+  if (len == sizeof(SensorPacket)) {
     memcpy(&recievedData, data, sizeof(SensorPacket));
-
-    Serial.println("Recieved from: ");
-    Serial.println(recievedData.name);
-    Serial.println("moisty level: ");
-    Serial.println(recievedData.humidity);
-    Serial.println("Temperature: ");
-    Serial.println(recievedData.temperature);
-
-  }else{
-    Serial.println("packet size error, trying to print anyway");
-    Serial.println(recievedData.humidity);
+    Serial.println("Recebido de: " + String(recievedData.name));
+    Serial.println("Humidade: " + String(recievedData.humidity) + "%");
+    Serial.println("Temperatura: " + String(recievedData.temperature) + "°C");
+  } else {
+    Serial.println("Erro de tamanho de pacote");
   }
 }
 
 void handleRoot() {
-  String page = "<html><head><meta charset='utf-8'><meta http-equiv='refresh' content='2'><title>ESP-SAP</title></head><body>";
-  page += "<h1>Sensor Data</h1>";
-  page += "<p><b>Device:</b> " + String(recievedData.name) + "</p>";
-  page += "<p><b>Humidity:</b> " + String(recievedData.humidity) + "%</p>";
-  page += "<p><b>Temperature:</b> " + String(recievedData.temperature) + "°C</p>";
-  page += "</body></html>";
-
-  server.send(200, "text/html", page);
-}
-
-
-
-void setup(){
-  Serial.begin(9600);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(NN, PW);
-
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (esp_now_init() != 0) {
-    Serial.println("ESP-NOW init failed!");
+  File file = SPIFFS.open("/dashboard.html", "r");
+  if (!file) {
+    server.send(500, "text/plain", "Erro carregando arquivo");
     return;
   }
-
-  // Registra função para tratar pacotes recebidos
-  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE); // ESP8266
-  esp_now_register_recv_cb(onReceive);
-  Serial.println("workingig");
-
-
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println("Web server started!");
-
-
+  
+  String html = file.readString();
+  file.close();
+  
+  // Replace placeholders with live data
+  html.replace("{DEVICE}", String(recievedData.name));
+  html.replace("{HUMIDITY}", String(recievedData.humidity));
+  html.replace("{TEMP}", String(recievedData.temperature));
+  
+  server.send(200, "text/html", html);
 }
-void loop(){
-  server.handleClient();
-}
-
-
-/*
-
-// Serve data on web page
-
 
 void setup() {
   Serial.begin(9600);
+  
+  // Initialize SPIFFS
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS falhou!");
+    while(1) yield(); // Halt if filesystem fails
+  }
 
-  // Connect to home Wi-Fi
+  // Connect to WiFi
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  Serial.print("Connecting to Wi-Fi");
+  WiFi.begin(NN, PW);
+  
+  Serial.print("Conectando ao Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
+  Serial.println("\nConectado! IP: " + WiFi.localIP().toString());
 
-  Serial.println();
-  Serial.println("WiFi connected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // Start ESP-NOW
+  // Initialize ESP-NOW
   if (esp_now_init() != 0) {
-    Serial.println("ESP-NOW init failed!");
+    Serial.println("ESP-NOW init falhou!");
     return;
   }
-
+  
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(onReceive);
 
-  // Setup web server
+  // Configure web server
   server.on("/", handleRoot);
   server.begin();
-  Serial.println("Web server started!");
+  Serial.println("Web server iniciou");
 }
 
 void loop() {
   server.handleClient();
 }
-*/
